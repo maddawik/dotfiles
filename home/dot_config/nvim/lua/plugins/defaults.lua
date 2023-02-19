33e111613091da3b748b8194dcaabbe460842111
Mawdac <edwardreillycomputers@gmail.com>
2023-02-19 17:17:46 -0500

    chore(nvim): update defaults for colorscheme and telescope

--- home/dot_config/nvim/lua/plugins/defaults.lua
+++ home/dot_config/nvim/lua/plugins/defaults.lua
@@ -4,7 +4,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 
@@ -33,22 +33,22 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
+    -- keys = {
+    --   -- add a keymap to browse plugin files
+    --   -- stylua: ignore
+    --   {
+    --     "<leader>fp",
+    --     function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+    --     desc = "Find Plugin File",
+    --   },
+    -- },
     -- change some options
     opts = {
       defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
+        -- layout_strategy = "horizontal",
+        -- layout_config = { prompt_position = "top" },
+        -- sorting_strategy = "ascending",
+        winblend = 7,
       },
     },
   },
