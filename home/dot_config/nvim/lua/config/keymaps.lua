0668f0118003cdb5e17ee5925fc0eb396039ef72
Mawdac <edwardreillycomputers@gmail.com>
2023-02-20 18:26:18 -0500

    refactor(nvim): use local function for keymaps

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,29 +1,39 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-local opts = { silent = true }
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
 -- Exit insert mode with jk
-vim.keymap.set("i", "jk", "<ESC>", opts)
+map("i", "jk", "<ESC>")
+map("t", "jk", [[<C-\><C-n>]])
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<cmd>SmartCursorMoveLeft<cr>", opts)
-vim.keymap.set("n", "<C-j>", "<cmd>SmartCursorMoveDown<cr>", opts)
-vim.keymap.set("n", "<C-k>", "<cmd>SmartCursorMoveUp<cr>", opts)
-vim.keymap.set("n", "<C-l>", "<cmd>SmartCursorMoveRight<cr>", opts)
+map("n", "<C-h>", "<cmd>SmartCursorMoveLeft<cr>")
+map("n", "<C-j>", "<cmd>SmartCursorMoveDown<cr>")
+map("n", "<C-k>", "<cmd>SmartCursorMoveUp<cr>")
+map("n", "<C-l>", "<cmd>SmartCursorMoveRight<cr>")
 
 -- Better terminal navigation
-vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-vim.keymap.set("t", "<C-h>", [[<cmd>wincmd h<CR>]], opts)
-vim.keymap.set("t", "<C-j>", [[<cmd>wincmd j<CR>]], opts)
-vim.keymap.set("t", "<C-k>", [[<cmd>wincmd k<CR>]], opts)
-vim.keymap.set("t", "<C-l>", [[<cmd>wincmd l<CR>]], opts)
+map("t", "<C-h>", [[<cmd>wincmd h<CR>]])
+map("t", "<C-j>", [[<cmd>wincmd j<CR>]])
+map("t", "<C-k>", [[<cmd>wincmd k<CR>]])
+map("t", "<C-l>", [[<cmd>wincmd l<CR>]])
 
 -- Resize with arrows
-vim.keymap.set("n", "<C-Up>", "<cmd>SmartResizeUp<cr>", opts)
-vim.keymap.set("n", "<C-Down>", "<cmd>SmartResizeDown<cr>", opts)
-vim.keymap.set("n", "<C-Left>", "<cmd>SmartResizeLeft<cr>", opts)
-vim.keymap.set("n", "<C-Right>", "<cmd>SmartResizeRight<cr>", opts)
+map("n", "<C-Up>", "<cmd>SmartResizeUp<cr>")
+map("n", "<C-Down>", "<cmd>SmartResizeDown<cr>")
+map("n", "<C-Left>", "<cmd>SmartResizeLeft<cr>")
+map("n", "<C-Right>", "<cmd>SmartResizeRight<cr>")
 
 -- Code-folding
-vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+map("n", "zR", require("ufo").openAllFolds)
+map("n", "zM", require("ufo").closeAllFolds)
