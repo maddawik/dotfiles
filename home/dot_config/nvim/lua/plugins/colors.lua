b5dcc8ead5c12ef525926194d56f44bdd572e63b
Mawdac <ereilly@lookingglasscyber.com>
2023-02-23 16:04:30 -0500

    style(nvim): add vim-gotham and format

--- home/dot_config/nvim/lua/plugins/colors.lua
+++ home/dot_config/nvim/lua/plugins/colors.lua
@@ -1,10 +1,11 @@
 return {
   -- colorschemes
-  { "ellisonleao/gruvbox.nvim", lazy = false },
-  { "folke/tokyonight.nvim", lazy = false },
-  { "overcache/NeoSolarized", lazy = false },
-  { "rebelot/kanagawa.nvim", lazy = false },
+  { "ellisonleao/gruvbox.nvim",         lazy = false },
+  { "folke/tokyonight.nvim",            lazy = false },
+  { "overcache/neosolarized",           lazy = false },
+  { "rebelot/kanagawa.nvim",            lazy = false },
   { "nyoom-engineering/oxocarbon.nvim", lazy = false },
+  { "whatyouhide/vim-gotham",           lazy = false },
 
   -- set default colors
   {
