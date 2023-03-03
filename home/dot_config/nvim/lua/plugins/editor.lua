ec2bdcb32fd721f3e2b693e258c183ae5ee22ff7
Mawdac <ereilly@lookingglasscyber.com>
2023-03-03 10:44:45 -0500

    feat(nvim): add telescope-symbols and bump

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -58,11 +58,19 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-symbols.nvim",
+        keys = {
+          { "<leader>se", "<cmd>Telescope symbols<cr>", desc = "Emojis" }
+        },
+      }
     },
     opts = {
       defaults = {
