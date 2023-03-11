26ba4d5ee2889cc490f0a38df4ce98ceed0fc551
Mawdac <edwardreillycomputers@gmail.com>
2023-03-10 22:10:09 -0500

    chore(nvim): add more zettelkasten hotkeys
    chore(nvim): move hotkeys with rest of config

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -20,20 +20,3 @@ vim.keymap.set("n", "<C-Up>", "<cmd>SmartResizeUp<CR>")
 vim.keymap.set("n", "<C-Down>", "<cmd>SmartResizeDown<CR>")
 vim.keymap.set("n", "<C-Left>", "<cmd>SmartResizeLeft<CR>")
 vim.keymap.set("n", "<C-Right>", "<cmd>SmartResizeRight<CR>")
-
--- Telekasten
-require("which-key").register({
-  z = {
-    name = "+telekasten",
-    p = { "<cmd>Telekasten panel<CR>", "Command palette" },
-    f = { "<cmd>Telekasten find_notes<CR>", "Find Notes" },
-    g = { "<cmd>Telekasten search_notes<CR>", "Grep Notes" },
-    n = { "<cmd>Telekasten new_note<CR>", "New Note" },
-    d = { "<cmd>Telekasten goto_today<CR>", "Todays Note" },
-    w = { "<cmd>Telekasten goto_thisweek<CR>", "Weeks Note" },
-    z = { "<cmd>Telekasten follow_link<CR>", "Follow Link" },
-    b = { "<cmd>Telekasten show_backlinks<CR>", "Show Backlinks" },
-    c = { "<cmd>Telekasten show_calendar<CR>", "Show Calendar" },
-    t = { "<cmd>Telekasten toggle_todo<CR>", "Toggle Todo" },
-  },
-}, { prefix = "<leader>" })
--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -2,21 +2,6 @@
 local Util = require("lazyvim.util")
 
 return {
-  -- Telekasten - wiki
-  {
-    'renerocksai/telekasten.nvim',
-    dependencies = {
-      "renerocksai/calendar-vim"
-    },
-    cmd = "Telekasten",
-    opts = {
-      home = vim.fn.expand("~/zettelkasten"),
-      template_new_note = vim.fn.expand("~/zettelkasten/templates/basenote.md"),
-      template_new_daily = vim.fn.expand("~/zettelkasten/templates/daily.md"),
-      template_new_weekly = vim.fn.expand("~/zettelkasten/templates/weekly.md"),
-    },
-  },
-
   -- Custom dashboard
   {
     "goolord/alpha-nvim",
@@ -39,7 +24,7 @@ return {
     },
   },
 
-  -- better splits and tmux navigation
+  -- Better splits and tmux navigation
   {
     "mrjones2014/smart-splits.nvim",
     event = "VeryLazy",
@@ -128,7 +113,6 @@ return {
   -- Diffview
   {
     "sindrets/diffview.nvim",
-    cmd = "DiffviewOpen",
     config = true,
     keys = {
       { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" }
@@ -138,7 +122,6 @@ return {
   -- Symbol outline
   {
     "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
     keys = {
       { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" }
     },
@@ -264,14 +247,56 @@ return {
               color = fg("Constant"),
             },
             { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Statement") },
-            {
-              "fileformat",
-              padding = { left = 1, right = 2 },
-            },
+            { "encoding", },
+            { "fileformat", },
+          },
+          lualine_z = {
+            function()
+              local time = os.date("%I:%M %p")
+              return " " .. time
+            end,
           },
         }
       }
     end,
+  },
+
+  -- Zettelkasten
+  {
+    'renerocksai/telekasten.nvim',
+    dependencies = {
+      {
+        "renerocksai/calendar-vim",
+        event = "VeryLazy"
+      }
+    },
+    keys = {
+      { "<leader>zb", "<cmd>Telekasten show_backlinks<CR>",     desc = "Show Backlinks" },
+      { "<leader>zc", "<cmd>Telekasten show_calendar<CR>",      desc = "Show Calendar" },
+      { "<leader>zC", "<cmd>CalendarT<CR>",                     desc = "Full Calendar" },
+      { "<leader>zd", "<cmd>Telekasten goto_today<CR>",         desc = "Todays Note" },
+      { "<leader>zf", "<cmd>Telekasten find_notes<CR>",         desc = "Find Notes" },
+      { "<leader>zF", "<cmd>Telekasten find_friend_notes<CR>",  desc = "Friend Notes" },
+      { "<leader>zg", "<cmd>Telekasten search_notes<CR>",       desc = "Grep Notes" },
+      { "<leader>zi", "<cmd>Telekasten insert_link<CR>",        desc = "Insert Link" },
+      { "<leader>zn", "<cmd>Telekasten new_note<CR>",           desc = "New Note" },
+      { "<leader>zN", "<cmd>Telekasten new_templated_note<CR>", desc = "Templated Note" },
+      { "<leader>zp", "<cmd>Telekasten panel<CR>",              desc = "Command Palette" },
+      { "<leader>zr", "<cmd>Telekasten rename_note<CR>",        desc = "Rename Note" },
+      { "<leader>zt", "<cmd>Telekasten toggle_todo<CR>",        desc = "Toggle Todo" },
+      { "<leader>zw", "<cmd>Telekasten goto_thisweek<CR>",      desc = "Weeks Note" },
+      { "<leader>zy", "<cmd>Telekasten yank_link_to_note<CR>",  desc = "Yank Link" },
+      { "<leader>zz", "<cmd>Telekasten follow_link<CR>",        desc = "Follow Link" },
+    },
+    init = function()
+      require("which-key").register({ z = { name = "+zettelkasten", }, }, { prefix = "<leader>" })
+    end,
+    opts = {
+      home = vim.fn.expand("~/zettelkasten"),
+      template_new_note = vim.fn.expand("~/zettelkasten/templates/basenote.md"),
+      template_new_daily = vim.fn.expand("~/zettelkasten/templates/daily.md"),
+      template_new_weekly = vim.fn.expand("~/zettelkasten/templates/weekly.md"),
+    },
   },
 
   -- Winbar
