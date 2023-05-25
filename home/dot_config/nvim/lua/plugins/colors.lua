64cba932cd0cca8527fb9593e24f8403382aa1b4
Mawdac <edwardreillycomputers@gmail.com>
2023-05-25 16:25:22 -0400

    style(alacritty): more opaque

--- home/dot_config/nvim/lua/plugins/colors.lua
+++ home/dot_config/nvim/lua/plugins/colors.lua
@@ -1,13 +1,21 @@
 -- Colors
 return {
   { "ellisonleao/gruvbox.nvim",         event = "VeryLazy" },
-  { "folke/tokyonight.nvim",            event = "VeryLazy" },
   { "overcache/neosolarized",           event = "VeryLazy" },
   { "rebelot/kanagawa.nvim",            event = "VeryLazy" },
   { "catppuccin/nvim",                  event = "VeryLazy" },
   { "nyoom-engineering/oxocarbon.nvim", event = "VeryLazy" },
   { "whatyouhide/vim-gotham",           event = "VeryLazy" },
-
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   -- set default colors
   {
     "LazyVim/LazyVim",
