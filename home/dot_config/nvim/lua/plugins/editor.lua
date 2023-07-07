5df311e0c665aea5df2d57cf2661832e15174fd1
Mawdac <edwardreillycomputers@gmail.com>
2023-07-07 11:32:10 -0400

    feat(nvim): make surround keys easier to reach

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -59,6 +59,21 @@ return {
     },
   },
 
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+
   -- Telescope
   {
     "nvim-telescope/telescope.nvim",
