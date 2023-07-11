46ba0744d41386b6d98fb88d0337cf826db36df9
Mawdac <edwardreillycomputers@gmail.com>
2023-07-11 08:54:28 -0400

    fix(nvim): add progress/location elements back to lualine

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -267,9 +267,8 @@ return {
             },
           },
           lualine_y = {
-            {
-              "progress",
-            },
+            { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
           },
           lualine_z = {
             {
