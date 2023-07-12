5093952638c556eedeba0d0465e8f0692fafbaf6
Mawdac <edwardreillycomputers@gmail.com>
2023-07-12 12:58:21 -0400

    feat(nvim): add markdown preview babyyy

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -339,9 +339,26 @@ return {
     },
   },
 
+  -- Super GitHub Integration
   {
     "pwntester/octo.nvim",
     config = true,
     cmd = "Octo",
   },
+
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      require("which-key").register({ m = { name = "+markdown", }, }, { prefix = "<leader>" })
+    end,
+    ft = { "markdown" },
+    keys = {
+      { "<leader>mp", "<esc><cmd>MarkdownPreview<cr>",       desc = "Preview" },
+      { "<leader>mt", "<esc><cmd>MarkdownPreviewToggle<cr>", desc = "Toggle" },
+      { "<leader>ms", "<esc><cmd>MarkdownPreviewStop<cr>",   desc = "Stop" },
+    }
+  }
 }
