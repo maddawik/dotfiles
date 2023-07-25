065eafb9966bf4bf3517222e3607eadcecd57584
Mawdac <edwardreillycomputers@gmail.com>
2023-07-25 14:10:46 -0400

    style(nvim): remove autocommands for extra characters/number styles between modes

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -23,35 +23,35 @@ create_autocmd("FileType", {
   desc = "Change indenting for fish files",
 })
 
-local set_toggle = augroup("set_toggle")
-create_autocmd("InsertEnter", {
-  callback = function()
-    if vim.bo.filetype ~= "alpha"
-        and vim.bo.filetype ~= "neo-tree"
-        and vim.bo.filetype ~= "TelescopePrompt"
-        and vim.bo.filetype ~= "mason"
-        and vim.bo.filetype ~= "help"
-        and vim.bo.filetype ~= "Trouble"
-        and vim.bo.filetype ~= "SidebarNvim" then
-      vim.opt.relativenumber = true
-      vim.opt.list = true
-    end
-  end,
-  group = set_toggle,
-})
-
-create_autocmd({ "VimEnter", "BufEnter", "InsertLeave" }, {
-  callback = function()
-    if vim.bo.filetype ~= "alpha"
-        and vim.bo.filetype ~= "neo-tree"
-        and vim.bo.filetype ~= "TelescopePrompt"
-        and vim.bo.filetype ~= "mason"
-        and vim.bo.filetype ~= "help"
-        and vim.bo.filetype ~= "Trouble"
-        and vim.bo.filetype ~= "SidebarNvim" then
-      vim.opt.relativenumber = false
-      vim.opt.list = false
-    end
-  end,
-  group = set_toggle,
-})
+-- local set_toggle = augroup("set_toggle")
+-- create_autocmd("InsertEnter", {
+--   callback = function()
+--     if vim.bo.filetype ~= "alpha"
+--         and vim.bo.filetype ~= "neo-tree"
+--         and vim.bo.filetype ~= "TelescopePrompt"
+--         and vim.bo.filetype ~= "mason"
+--         and vim.bo.filetype ~= "help"
+--         and vim.bo.filetype ~= "Trouble"
+--         and vim.bo.filetype ~= "SidebarNvim" then
+--       vim.opt.relativenumber = true
+--       vim.opt.list = true
+--     end
+--   end,
+--   group = set_toggle,
+-- })
+--
+-- create_autocmd({ "VimEnter", "BufEnter", "InsertLeave" }, {
+--   callback = function()
+--     if vim.bo.filetype ~= "alpha"
+--         and vim.bo.filetype ~= "neo-tree"
+--         and vim.bo.filetype ~= "TelescopePrompt"
+--         and vim.bo.filetype ~= "mason"
+--         and vim.bo.filetype ~= "help"
+--         and vim.bo.filetype ~= "Trouble"
+--         and vim.bo.filetype ~= "SidebarNvim" then
+--       vim.opt.relativenumber = false
+--       vim.opt.list = false
+--     end
+--   end,
+--   group = set_toggle,
+-- })
--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -3,13 +3,13 @@ opt.colorcolumn = "80"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 20
 opt.foldmethod = "expr"
-opt.listchars = {
-  space = ".",
-  eol = "↲",
-  nbsp = "␣",
-  trail = "·",
-  precedes = "←",
-  extends = "→",
-  tab = "¬ ",
-  conceal = "※",
-}
+-- opt.listchars = {
+--   space = ".",
+--   eol = "↲",
+--   nbsp = "␣",
+--   trail = "·",
+--   precedes = "←",
+--   extends = "→",
+--   tab = "¬ ",
+--   conceal = "※",
+-- }
