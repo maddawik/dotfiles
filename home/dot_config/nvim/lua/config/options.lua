3ba59121e23d8687ca92175a64a3707a120f60c2
Mawdac <edwardreillycomputers@gmail.com>
2023-10-06 16:15:22 -0400

    chore(nvim): remove treesitter fold options

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,15 +1,1 @@
-local opt = vim.opt
-opt.colorcolumn = "80"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldlevel = 20
-opt.foldmethod = "expr"
--- opt.listchars = {
---   space = ".",
---   eol = "↲",
---   nbsp = "␣",
---   trail = "·",
---   precedes = "←",
---   extends = "→",
---   tab = "¬ ",
---   conceal = "※",
--- }
+vim.opt.colorcolumn = "80"
