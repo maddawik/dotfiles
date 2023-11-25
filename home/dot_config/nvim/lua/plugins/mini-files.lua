2985330221ddc2719a1b29931b1a8f8bcffa8836
Mawdac <edwardreillycomputers@gmail.com>
2023-11-25 10:18:08 -0500

    fix(nvim): update mini.files keymaps

--- home/dot_config/nvim/lua/plugins/mini-files.lua
+++ home/dot_config/nvim/lua/plugins/mini-files.lua
@@ -1,31 +1,44 @@
+local minifiles_toggle = function(...)
+  if not MiniFiles.close() then
+    MiniFiles.open(...)
+  end
+end
+
 return {
   "echasnovski/mini.files",
   opts = {
+    mappings = {
+      synchronize = "<CR>",
+    },
     options = {
       use_as_default_explorer = true,
     },
   },
-  keys = {
-    {
-      "-",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open files (directory of current file)",
-    },
-    {
-      "<leader>e",
-      function()
-        require("mini.files").open(require("lazyvim.util.root").get(), true)
-      end,
-      desc = "Open files (root dir)",
-    },
-    {
-      "<leader>E",
-      function()
-        require("mini.files").open(vim.loop.cwd(), true)
-      end,
-      desc = "Open files (cwd)",
-    },
-  },
+  -- Passing a function that returns the table of keymaps overrides any of the
+  -- defaults that LazyVim has (<leader>fm, and <leader>fM in this case)
+  keys = function()
+    return {
+      {
+        "-",
+        function()
+          minifiles_toggle(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open files (directory of current file)",
+      },
+      {
+        "<leader>e",
+        function()
+          minifiles_toggle(require("lazyvim.util.root").get(), true)
+        end,
+        desc = "Open files (cwd)",
+      },
+      {
+        "<leader>E",
+        function()
+          minifiles_toggle(nil, false)
+        end,
+        desc = "Open files (root dir)",
+      },
+    }
+  end,
 }
