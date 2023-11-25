928bbc2bb6f73c5edfcdb85d2e29cc6d1f750ba8
Mawdac <edwardreillycomputers@gmail.com>
2023-11-25 00:27:47 -0500

    feat(nvim): make mini.files THE file explorer

--- home/dot_config/nvim/lua/plugins/mini-files.lua
+++ home/dot_config/nvim/lua/plugins/mini-files.lua
@@ -1,12 +1,31 @@
 return {
   "echasnovski/mini.files",
+  opts = {
+    options = {
+      use_as_default_explorer = true,
+    },
+  },
   keys = {
     {
       "-",
       function()
         require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
-      desc = "Open mini.files (current file dir)",
+      desc = "Open files (directory of current file)",
+    },
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(require("lazyvim.util.root").get(), true)
+      end,
+      desc = "Open files (root dir)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.loop.cwd(), true)
+      end,
+      desc = "Open files (cwd)",
     },
   },
 }
