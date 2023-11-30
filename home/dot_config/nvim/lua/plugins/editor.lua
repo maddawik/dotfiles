77b8c03058249d40ab349f60d0df039a0b6838dd
Mawdac <edwardreillycomputers@gmail.com>
2023-11-30 16:07:13 -0500

    chore: minor cleanup

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -1,53 +1,53 @@
 return {
-  -- Scoped buffers
-  {
-    "tiagovla/scope.nvim",
-    config = true,
-    event = "VeryLazy",
-  },
+	-- Scoped buffers
+	{
+		"tiagovla/scope.nvim",
+		config = true,
+		event = "VeryLazy",
+	},
 
-  -- Better splits and tmux navigation
-  {
-    "mrjones2014/smart-splits.nvim",
-    event = "VeryLazy",
-  },
+	-- Better splits and tmux navigation
+	{
+		"mrjones2014/smart-splits.nvim",
+		event = "VeryLazy",
+	},
 
-  -- Super smart column status
-  {
-    "Bekaboo/deadcolumn.nvim",
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-  },
+	-- Super smart column status
+	{
+		"Bekaboo/deadcolumn.nvim",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+	},
 
-  -- Trim trailing whitespace
-  {
-    "lewis6991/spaceless.nvim",
-    config = true,
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-  },
+	-- Trim trailing whitespace
+	{
+		"lewis6991/spaceless.nvim",
+		config = true,
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+	},
 
-  -- Smart sort
-  {
-    "sQVe/sort.nvim",
-    config = true,
-    keys = {
-      { "<leader>S", "<esc><cmd>Sort<cr>", desc = "Sort", mode = "v" },
-      { "<leader>U", "<esc><cmd>Sort u<cr>", desc = "Unique Sort", mode = "v" },
-    },
-  },
+	-- Smart sort
+	{
+		"sQVe/sort.nvim",
+		config = true,
+		keys = {
+			{ "<leader>S", "<esc><cmd>Sort<cr>", desc = "Sort", mode = "v" },
+			{ "<leader>U", "<esc><cmd>Sort u<cr>", desc = "Unique Sort", mode = "v" },
+		},
+	},
 
-  -- Split/Join blocks of code
-  {
-    "Wansmer/treesj",
-    config = true,
-    keys = {
-      { "gS", "<esc><cmd>TSJSplit<cr>", desc = "Split node under cursor" },
-      { "gJ", "<esc><cmd>TSJJoin<cr>", desc = "Join node under cursor" },
-    },
-  },
+	-- Split/Join blocks of code
+	{
+		"Wansmer/treesj",
+		config = true,
+		keys = {
+			{ "gS", "<esc><cmd>TSJSplit<cr>", desc = "Split node under cursor" },
+			{ "gJ", "<esc><cmd>TSJJoin<cr>", desc = "Join node under cursor" },
+		},
+	},
 }
--- home/dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ home/dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,8 +1,6 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
-    "hrsh7th/cmp-emoji",
-    "chrisgrieser/cmp-nerdfont",
     { "mtoohey31/cmp-fish", ft = "fish" },
   },
   opts = function(_, opts)
@@ -16,8 +14,6 @@ return {
     })
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
       { name = "fish" },
-      { name = "emoji" },
-      { name = "nerdfont" },
     }))
   end,
 }
