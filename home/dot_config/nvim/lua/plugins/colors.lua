7a01b6e814cb793996b8b78027d228332972b547
Mawdac <ereilly@zerofox.com>
2023-12-05 09:07:59 -0500

    chore(nvim): cleanup

--- home/dot_config/nvim/lua/plugins/colors.lua
+++ home/dot_config/nvim/lua/plugins/colors.lua
@@ -1,12 +1,7 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim", event = "VeryLazy" },
-  -- { "overcache/neosolarized", event = "VeryLazy" },
-  -- { "nyoom-engineering/oxocarbon.nvim", event = "VeryLazy" },
-  -- { "EdenEast/nightfox.nvim", event = "VeryLazy" },
-  -- { "rebelot/kanagawa.nvim", event = "VeryLazy" },
+  { "rebelot/kanagawa.nvim", event = "VeryLazy" },
   {
     "catppuccin/nvim",
-    event = "VeryLazy",
     name = "catppuccin",
   },
   {
--- home/dot_config/nvim/lua/plugins/telekasten.lua
+++ home/dot_config/nvim/lua/plugins/telekasten.lua
@@ -1,19 +1,13 @@
 return {
   "renerocksai/telekasten.nvim",
   keys = {
-    { "<leader>zb", "<cmd>Telekasten show_backlinks<CR>", desc = "Show Backlinks" },
     { "<leader>zd", "<cmd>Telekasten goto_today<CR>", desc = "Todays Note" },
     { "<leader>zf", "<cmd>Telekasten find_notes<CR>", desc = "Find Notes" },
-    { "<leader>zF", "<cmd>Telekasten find_friend_notes<CR>", desc = "Friend Notes" },
     { "<leader>zg", "<cmd>Telekasten search_notes<CR>", desc = "Grep Notes" },
-    { "<leader>zi", "<cmd>Telekasten insert_link<CR>", desc = "Insert Link" },
     { "<leader>zn", "<cmd>Telekasten new_note<CR>", desc = "New Note" },
-    { "<leader>zN", "<cmd>Telekasten new_templated_note<CR>", desc = "Templated Note" },
     { "<leader>zp", "<cmd>Telekasten panel<CR>", desc = "Command Palette" },
     { "<leader>zr", "<cmd>Telekasten rename_note<CR>", desc = "Rename Note" },
-    { "<leader>zw", "<cmd>Telekasten goto_thisweek<CR>", desc = "Weeks Note" },
-    { "<leader>zy", "<cmd>Telekasten yank_link_to_note<CR>", desc = "Yank Link" },
-    { "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = "Follow Link" },
+    { "<leader>zt", "<cmd>Telekasten toggle_todo<CR>", desc = "Toggle Todo" },
   },
   init = function()
     require("which-key").register({ z = { name = "+zettelkasten" } }, { prefix = "<leader>" })
