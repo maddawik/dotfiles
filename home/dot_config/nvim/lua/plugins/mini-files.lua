9ecb40fd6754d1e0286cc7e89d0f7b83df412156
Mawdac <ereilly@zerofox.com>
2023-12-10 23:31:57 -0500

    fix(nvim): default to hiding dotfiles in mini.files

--- home/dot_config/nvim/lua/plugins/mini-files.lua
+++ home/dot_config/nvim/lua/plugins/mini-files.lua
@@ -4,9 +4,24 @@ local minifiles_toggle = function(...)
   end
 end
 
+local filter_show = function()
+  return true
+end
+local filter_hide = function(fs_entry)
+  return not vim.startswith(fs_entry.name, ".")
+end
+
 return {
   "echasnovski/mini.files",
   opts = {
+    content = {
+      filter = filter_hide,
+    },
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 30,
+    },
     mappings = {
       synchronize = "<CR>",
     },
@@ -14,6 +29,28 @@ return {
       use_as_default_explorer = true,
     },
   },
+  config = function(_, opts)
+    require("mini.files").setup(opts)
+
+    local show_dotfiles = function()
+      require("mini.files").refresh({ content = { filter = filter_show } })
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        vim.keymap.set("n", "g.", show_dotfiles, { buffer = buf_id })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesActionRename",
+      callback = function(event)
+        require("lazyvim.util").lsp.on_rename(event.data.from, event.data.to)
+      end,
+    })
+  end,
   -- Passing a function that returns the table of keymaps overrides any of the
   -- defaults that LazyVim has (<leader>fm, and <leader>fM in this case)
   keys = function()
