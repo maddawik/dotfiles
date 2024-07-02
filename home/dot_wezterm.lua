609cb0a7bc45d889c84c5c61c5b7444caaa6f0e9
Mawdac <edwardreillycomputers@gmail.com>
2024-07-01 21:53:00 -0400

    chore: format

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -27,10 +27,10 @@ config.audible_bell = "Disabled"
 config.font_size = 20
 config.enable_tab_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 config.window_decorations = "RESIZE"
 
