f741deef6c7efa89498b29a628367badb3a94ca0
maddawik <edwardreillycomputers@gmail.com>
2025-02-15 20:21:58 -0500

    fix: update for latest terragrunt, use cache

--- home/dot_config/private_fish/conf.d/terragrunt.fish
+++ home/dot_config/private_fish/conf.d/terragrunt.fish
@@ -2,16 +2,14 @@ if not status is-interactive
     exit
 end
 
-# terraform & terragrunt
+# terragrunt
 if not test -e "$HOME/.cache/"
     mkdir "$HOME/.cache/"
-end
-if not test -e "$HOME/.cache/tf-cache/"
-    mkdir "$HOME/.cache/tf-cache/"
 end
 if not test -e "$HOME/.cache/tg-cache/"
     mkdir "$HOME/.cache/tg-cache/"
 end
 
-set -gx TF_PLUGIN_CACHE_DIR "$HOME/.cache/tf-cache"
 set -gx TG_DOWNLOAD_DIR "$HOME/.cache/tg-cache"
+set -gx TG_PROVIDER_CACHE 1
+set -gx TG_PARALLELISM 1
