78f4a3580ccbf9c80a4e6645a122cdef70aa696d
maddawik <edwardreillycomputers@gmail.com>
2025-09-25 14:49:39 -0400

    fix(fish): update ls aliases

--- home/dot_config/private_fish/functions/la.fish
+++ home/dot_config/private_fish/functions/la.fish
@@ -1,4 +1,4 @@
 # Overrides the builtin fish function
 function la --wraps=ls --description 'List contents of directory, including hidden files in directory using long format'
-    ls -lA $argv
+    ls -A $argv
 end
