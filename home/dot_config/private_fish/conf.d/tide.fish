40675d4f8a0a5f52d9e8ff6f9d321f571bf57973
maddawik <edwardreillycomputers@gmail.com>
2025-12-19 16:34:41 -0500

    chore(fish): cleanup tide prompt

--- home/dot_config/private_fish/conf.d/tide.fish
+++ home/dot_config/private_fish/conf.d/tide.fish
@@ -4,9 +4,11 @@ end
 
 # tide prompt
 set -gx tide_git_icon ""
+set -gx tide_private_mode_icon "󰗹 "
 set -gx tide_git_truncation_length 0
-if not contains private_mode $tide_right_prompt_items
-    set -a tide_right_prompt_items private_mode
+
+if not contains private_mode $tide_left_prompt_items
+    set -p tide_left_prompt_items private_mode
 end
 set tide_pwd_color_dirs blue
 
