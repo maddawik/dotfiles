0c3a6baa3551a350af9773f692541fa670982334
maddawik <edwardreillycomputers@gmail.com>
2026-03-07 14:59:33 -0500

    chore(nvim): remove refactoring.nvim

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -39,7 +39,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "lazyvim.plugins.extras.editor.outline" },
-    { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
