dc6ebef1f7b55483bfd4a88fb219610fa833c9b5
maddawik <edwardreillycomputers@gmail.com>
2026-04-07 11:55:34 -0400

    feat: update for 0.12.1, improve mini.files set_cwd function

--- home/dot_config/nvim/lua/plugins/mini-files.lua
+++ home/dot_config/nvim/lua/plugins/mini-files.lua
@@ -19,7 +19,7 @@ return {
         -- works only if cursor is on the valid file system entry
         local grug_path = (MiniFiles.get_fs_entry() or {}).path
         if grug_path == nil then
-          return vim.notify("Cursor is not on valid entry", vim.log.levels.WARN)
+          return Snacks.notify.warn("Cursor is not on valid entry")
         end
         local prefills = { paths = vim.fs.dirname(grug_path) }
 
@@ -43,17 +43,32 @@ return {
       local yank_path = function()
         local yank_path = (require("mini.files").get_fs_entry() or {}).path
         if yank_path == nil then
-          return vim.notify("Cursor is not on valid entry", vim.log.levels.WARN)
+          return Snacks.notify.warn("Cursor is not on valid entry")
         end
         vim.fn.setreg(vim.v.register, yank_path)
-        vim.notify("Copied path to clipboard", vim.log.levels.INFO)
+        Snacks.notify.info("Copied path to clipboard")
+      end
+
+      -- Set focused directory as current working directory
+      local set_cwd = function()
+        local path = (require("mini.files").get_fs_entry() or {}).path
+        if path == nil then
+          return Snacks.notify.warn("Cursor is not on valid entry")
+        end
+        vim.fn.chdir(vim.fs.dirname(path))
+        Snacks.notify.info("cwd: " .. vim.fn.fnamemodify(vim.fs.dirname(path), ":~"))
       end
 
       vim.api.nvim_create_autocmd("User", {
         pattern = "MiniFilesBufferCreate",
         callback = function(args)
-          vim.keymap.set("n", "gy", yank_path, { buffer = args.data.buf_id, desc = "Yank path" })
-          vim.keymap.set("n", "gs", files_grug_far_replace, { buffer = args.data.buf_id, desc = "Search in directory" })
+          local b = args.data.buf_id
+          vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+          vim.keymap.set("n", "gs", files_grug_far_replace, { buffer = b, desc = "Search in directory" })
+          -- NOTE: This is to override the LazyVim keymap
+          vim.schedule(function()
+            vim.keymap.set("n", "gc", set_cwd, { buffer = b, desc = "Set cwd" })
+          end)
         end,
       })
 
