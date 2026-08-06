4bb63ea6fe3cfece5e3a2fa009ba46016886aeae
maddawik <edwardreillycomputers@gmail.com>
2026-08-06 10:30:18 -0400

    style(sketchybar): cleanup

--- home/dot_config/sketchybar/plugins/executable_aerospace.sh
+++ home/dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -3,8 +3,17 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
+TN_FG=0xffc8d3f5
+TN_MUTED=0xff828bb8
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on
+  sketchybar --set "$NAME" \
+    background.drawing=on \
+    background.border_color=0xff82aaff \
+    icon.color="$TN_FG"
 else
-  sketchybar --set $NAME background.drawing=off
+  sketchybar --set "$NAME" \
+    background.drawing=off \
+    background.border_color=0x0082aaff \
+    icon.color="$TN_MUTED"
 fi
