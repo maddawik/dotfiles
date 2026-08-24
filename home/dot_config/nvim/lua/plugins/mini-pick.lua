c25c053f67aa7a71737029be2f002c28b82e9841
maddawik <edwardreillycomputers@gmail.com>
2026-08-24 14:00:20 -0400

    fix(nvim): cleanup keys for mini.pick

--- home/dot_config/nvim/lua/plugins/mini-pick.lua
+++ home/dot_config/nvim/lua/plugins/mini-pick.lua
@@ -17,6 +17,27 @@ return {
             vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n", false)
           end,
         },
+        scroll_down_alt = {
+          char = "<C-d>",
+          func = function()
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-f>", true, true, true), "n", false)
+          end,
+        },
+        scroll_up_alt = {
+          char = "<C-u>",
+          func = function()
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-b>", true, true, true), "n", false)
+          end,
+        },
+        delete_left = "<C-z>",
+      },
+      window = {
+        config = { border = "rounded" },
+        prompt_caret = "▏",
+        prompt_prefix = " ",
+      },
+      options = {
+        use_cache = true,
       },
     },
     config = function(_, opts)
