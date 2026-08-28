9d1147ad1579ecf132243a43f4a5b3b7f0012197
maddawik <edwardreillycomputers@gmail.com>
2026-08-27 23:34:56 -0400

    feat(nvim): add svelte lsp config

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -210,6 +210,16 @@ return {
             },
           },
         },
+        svelte = {
+          on_attach = function(client, _)
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts" },
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+              end,
+            })
+          end,
+        },
         dockerls = {},
         docker_compose_language_service = {},
         fish_lsp = {},
